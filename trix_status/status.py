3b2e4717f5a5a78957f9b4f81df51c60f4971a5b
Dmitry Chirikov <dmitry@chirikov.ru>
2018-03-20 12:33:28 +0100

    Add mutex for output

--- trix_status/status.py
+++ trix_status/status.py
@@ -17,6 +17,7 @@ along with slurm_health_checker.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 from multiprocessing.pool import ThreadPool
+from multiprocessing import Lock
 
 from out import Out
 from healthstatus import HealthStatus
@@ -63,6 +64,7 @@ class TrixStatus(object):
         self.timeout = timeout
         self.log.debug('Start thread pool')
         thread_pool = ThreadPool(processes=self.fanout)
+        self.lock = Lock()
 
         self.log.debug('Map main workers to threads')
         workers_return = thread_pool.map(self.main_worker, self.nodes)
@@ -100,7 +102,9 @@ class TrixStatus(object):
         self.log.debug(
             '{}:Retuned from check workers: {}'.format(node, workers_return))
 
-        self.out.line(node, workers_return)
+        with self.lock:
+            self.out.line(node, workers_return)
+
         return True
 
     def check_worker(self, check):
